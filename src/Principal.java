2e259a1d2e579adddff342f7b67e417eb0ca0072
unknown <DAM107@iesataulfo.local>
2024-04-16 15:33:12 +0200

    Segunda fase proyecto (Clase1 creada y Principal creada y comentada)

--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,7 @@
 
 public class Principal {
 	public static void main(String[] args) {
-        // Crear un objeto de la clase Clase1
+        // Crear un objeto de la clase Clase1.
         Clase1 objClase1 = new Clase1();
     }
 }
