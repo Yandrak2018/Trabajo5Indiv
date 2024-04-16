0f0d2f3f4b46ec6027c6d35e5aece90fbd26f385
unknown <DAM107@iesataulfo.local>
2024-04-16 15:41:34 +0200

    Primera fase proyecto (Clase1 y Principal creadas)

--- src/Clase1.java
+++ src/Clase1.java
@@ -1,7 +1,3 @@
 
 public class Clase1 {
-    // Constructor por defecto.
-	 public Clase1() {
-	        System.out.println("Soy un objeto de la clase Clase1 y acabo de ser creado");
-	 }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,3 @@
 
 public class Principal {
-	public static void main(String[] args) {
-        // Crear un objeto de la clase Clase1.
-        Clase1 objClase1 = new Clase1();
-    }
 }
