8a791f0b088c98e19114decc608b1a605ac83519
unknown <DAM107@iesataulfo.local>
2024-04-16 15:45:36 +0200

    Cuarta fase proyecto (Clase1 creada, comentada y codificada y Principal creada y comentada)

--- src/Clase1.java
+++ src/Clase1.java
@@ -1,4 +1,7 @@
 
 public class Clase1 {
-	//tiene un constructor por defecto que muestra por pantalla "Soy un objeto de la clase y acabo de ser creado" 
+	//tiene un constructor por defecto que muestra por pantalla "Soy un objeto de la clase y acabo de ser creado"
+	public Clase1() {
+        System.out.println("Soy un objeto de la clase Clase1 y acabo de ser creado");
+    }
 }
